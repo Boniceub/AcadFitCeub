664abd3836356d26fc165616286ca705f7e17f67
Pedro <pedrovirmond@gmail.com>
2026-05-18 17:48:27 -0300

    feat: controller e rotas de exercicios (GET /exercicios)

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 const authRoutes = require("./src/routes/authRoutes");
 const usuarioRoutes = require("./src/routes/usuarioRoutes");
+const exercicioRoutes = require("./src/routes/exercicioRoutes");
 
 const app = express();
 app.use(cors());
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/usuario", usuarioRoutes);
+app.use("/exercicios", exercicioRoutes);
 
 app.get("/", (req, res) => res.json({ message: "AcadFitCeub API rodando!" }));
 
